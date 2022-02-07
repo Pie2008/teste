0e23ae9dec64b72d6d346aa8a1167172cf4776a3
Pie2008 <97923490+Pie2008@users.noreply.github.com>
2022-02-07 16:21:25 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var Student
+function setup() {
+  createCanvas(400,400);
+  Student=new Student("Lucas", 15, "apostila")
+  Student.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+   constructor(name, ade, book){
+       this.name=name
+       this.ade=ade
+       this.book=book
+    }
+    display(){
+        console.log(this.name)
+        console.log(this.ade)
+        console.log(this.book)
+    }
+}
+
+
+
+
